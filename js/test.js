b622b7d06760a497de646af47dd63f5f7fdc3a4e
hacatonchik <56761966+hacatonchik@users.noreply.github.com>
2019-10-21 00:11:18 +0500

    Add files via upload

--- js/test.js
+++ js/test.js
@@ -1,1 +1,24 @@
-function pizda(){}
+window.onload = function(){
+  $('.enter-text-chat-bot').click( function(){
+    pizda($('#input-text-chat-bot').val());
+  });
+};
+function pizda(str){
+  let words = new Array();
+  var text1 = str.toLowerCase();
+  var separator = ' ';
+  words = text1.split(separator);
+  var bufStr;
+    for(var i=0;i<words.length;i++){
+      bufStr = words[i];
+      if(bufStr[0].toLowerCase() == bufStr[0].toUpperCase()){
+        words[i] = words[i].split(bufStr[0]).join('');
+      }
+      if(bufStr[bufStr.length-1].toLowerCase() == bufStr[bufStr.length-1].toUpperCase()){
+        words[i] = words[i].split(bufStr[bufStr.length-1]).join('');
+      }
+    }
+  for(var i=0;i<words.length;i++){
+    alert(words[i]);
+  }
+}
