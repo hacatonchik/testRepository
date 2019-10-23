1dc3cb04db38501f8d09dd66a7d895534029e76b
hacatonchik <56761966+hacatonchik@users.noreply.github.com>
2019-10-23 15:48:24 +0500

    Add files via upload

--- js/test.js
+++ js/test.js
@@ -9,31 +9,18 @@
   });
   */
 };
-function WordsArray(str){
+function WordsArray(keyString){
   let words = new Array();
-  var text1 = str.toLowerCase();
+  keyString = keyString.toLowerCase();
   var separator = ' ';
-  words = text1.split(separator);
-  var bufStr;
-    for(var i=0;i<words.length;i++){
-      bufStr = words[i];
-      for(var j=0;j<bufStr.length;j++){
-        if(bufStr[j] == bufStr[j].toUpperCase()){
-          if(j == 0 || j ==  bufStr.length-1) words[i] = words[i].split(bufStr[j]).join('');
-          else{
-            let bufArr = new Array();
-            words[i] = words[i].split(bufStr[j]).join(' ');
-            bufArr = words[i].split(separator);
-            words[i] = bufArr[0];
-            for(var l=1;l<bufArr.length;l++)
-              words.push(bufArr[l]);
-          }
-        }
-      }
-      if(words[i] == words[i].toUpperCase()) words.splice(i, 1);
+  var stringKey = "";
+  for(var i=0;i<keyString.length;i++){
+    if(keyString[i] != keyString[i].toUpperCase()){
+      stringKey+=keyString[i];
+    }else if(i!=0 && keyString[i-1] != keyString[i-1].toUpperCase()){
+      words.push(stringKey);
+      stringKey="";
     }
-    for(var i=0;i<words.length;i++){
-      if(words[i] == words[i].toUpperCase()) words.splice(i, 1);
-    }
-  for(var i=0;i<words.length;i++) console.log(words[i]);
+  }
+  return words;
 }
