21e4304719a335c7cdb247833aae1a3e00b61f7f
hacatonchik <56761966+hacatonchik@users.noreply.github.com>
2019-10-23 20:57:07 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+﻿window.onload = function(){
+  $('.enter-text-chat-bot').click( function(){
+    WordsArray($('.input-text-chat-bot').val());
+  });
+  /*
+    типо сюда сможешь указать класс твоего текст ареа и нужной тебе кнопки
+  $('.enter-text-chat-bot').click( function(){
+    WordsArray($('.input-text-chat-bot').val());
+  });
+  */
+};
+function WordsArray(keyString){
+  let words = new Array();
+  keyString = keyString.toLowerCase();
+  var separator = ' ';
+  var stringKey = "";
+  for(var i=0;i<keyString.length;i++){
+    if(keyString[i] != keyString[i].toUpperCase()){
+      stringKey+=keyString[i];
+    }else if(i!=0 && keyString[i-1] != keyString[i-1].toUpperCase()){
+      words.push(stringKey);
+      stringKey="";
+    }
+  }
+  return words;
+}
