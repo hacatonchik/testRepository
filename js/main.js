582b0a9fd2eecb90dca93257a2713be604475c1f
hacatonchik <56761966+hacatonchik@users.noreply.github.com>
2019-10-24 21:30:03 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,23 +1,61 @@
 ﻿$(document).ready(function(){
   $('.enter-text-chat-bot').click( function(){
-    //WordsArray($('.input-text-chat-bot').val());
-    AddSpan(1, $('.input-text-chat-bot').val());
+    onUpdate();
   });
   $(document).on('keypress',function(e) {
     if(e.which == 13) {
-        AddSpan(1, $('.input-text-chat-bot').val());
+      onUpdate();
     }
+
 });
-  /*
-    типо сюда сможешь указать класс твоего текст ареа и нужной тебе кнопки
-  $('.enter-text-chat-bot').click( function(){
-    WordsArray($('.input-text-chat-bot').val());
+
+var config = {
+apiKey: "AIzaSyAl05TmTxUFYOfpnKov_BUgZUD090OrIFU",
+authDomain: "disobeyparrot.firebaseapp.com",
+databaseURL: "https://disobeyparrot.firebaseio.com",
+storageBucket: "disobeyparrot.appspot.com"
+};
+firebase.initializeApp(config);
+var ref = firebase.database().ref();
+
+function onUpdate(){
+  var textareaText=$("textarea.input-text-chat-bot").val();
+jQuery('textarea').blur();
+
+  let wordsAnswer = WordsArray(textareaText);
+  if(wordsAnswer.length!=0){
+    AddSpan(1,textareaText);
+  }
+  var countAnswer = "";
+  for(var i=0;i<wordsAnswer.length;i++){
+
+
+    if(countAnswer!=wordsAnswer[i]){
+    getAnswerBot(wordsAnswer[i]);}
+countAnswer=wordsAnswer[i];
+  }
+}
+
+function getAnswerBot(keys){
+  ref.child("chatBot").child(keys).child("answer").once("value").then(function(snapshot) {
+    var result = snapshot.val();
+      if(result!=null){
+
+        dbAnswer(result);
+      }
   });
-  */
+}
+
+function dbAnswer(answer){
+  AddSpan(0,answer);
+
+}
+
 });
 function WordsArray(keyString){
   let words = new Array();
   keyString = keyString.toLowerCase();
+  keyString+=" ";
   var stringKey = "";
   for(var i=0;i<keyString.length;i++){
     if(keyString[i] != keyString[i].toUpperCase()){
@@ -29,13 +67,16 @@ function WordsArray(keyString){
   }
   return words;
 }
+
+
 function AddSpan(number, inputString){
+  inputString[0] = inputString[0].toUpperCase();
   if(number == 1){
     $('.write-text-chat-bot').append($("<div class='output-users-text'><span class='output-users-text-span'>"+inputString+"</span></div>"));
   }
   if(number == 0){
-    $('.write-text-chat-bot').append($("<div class='output-chat-bot-text'><span class='output-chat-bot-text-span'>"+WordsArray(inputString)+"</span></div>"));
+    $('.write-text-chat-bot').append($("<div class='output-chat-bot-text'><span class='output-chat-bot-text-span'>"+ inputString+"</span></div>"));
   }
-  jQuery('textarea').blur();
+
   $('.input-text-chat-bot').val("");
 }
